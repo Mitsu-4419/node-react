780f0e2b5790600e5f9da62e4b88e915a6259ffe
Mitsutomo Yamada <Mitsu44@Mitsutomo-no-MacBook-Air.local>
2021-05-01 09:58:47 +0900

    third commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports={
+    googleClientID:'1028810032940-mhd3obq89e5tha2pelbko9c8ulk7qk03.apps.googleusercontent.com',
+    googleClientSecret:'SvVKBWEhUv3tzSkGvO4LuqKR',
+    mongoURI:'mongodb+srv://Ohagex:LdnwMomEFkkJa7ZE@emaily.zpe4f.mongodb.net/Emaily?retryWrites=true&w=majority',
+    cookieKEy:'reacted',
+}
+
+// Ohagex
+// LdnwMomEFkkJa7ZE
